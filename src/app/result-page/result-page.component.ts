e5defa6ebd21712d4c155b87d28211629103e669
Михаил Козлов <mixa@Mihails-MacBook-Pro.local>
2019-05-19 15:00:03 +0300

    Update result page

--- src/app/result-page/result-page.component.ts
+++ src/app/result-page/result-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-result-page',
@@ -6,7 +7,24 @@ import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./result-page.component.styl']
 })
 export class ResultPageComponent implements OnInit {
-  @Input() score: string;
+  @Input() score: number;
+  @Input() total: number;
+
+  GetResultText(): string {
+    switch (this.score) {
+      case 1 || 2:
+        return '«Мы все учились понемногу,<br />Чему-нибудь и как-нибудь».<br />Попробуйте еще.';
+      case 3:
+        return '«Ах, обмануть меня нетрудно,<br />Я сам обманываться рад!»<br />Будьте внимательнее.';
+      case 4:
+        return 'Старик державин вас заметил 😉';
+      case 5:
+        return 'В человеке всё должно быть прекрасно — как в вас!';
+      default:
+        return 'Старик державин вас заметил 😉';
+    }
+  }
+
   constructor() {}
 
   ngOnInit() {}
